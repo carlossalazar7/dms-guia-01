f9929fb021b321186a5ad79e073f7385c6a4e41e
carlossalazar7 <carlossalazar2228@gmail.com>
2024-01-24 08:52:24 -0600

    first commit

---
+++ src/udb/edu/sv/TiposDatos.java
@@ -0,0 +1,30 @@
+package udb.edu.sv;
+
+public class TiposDatos {
+    /*
+
+    Asignatura Java Avanzada
+    Programa de ejempo de tipo de datos en Java
+
+    */
+
+    public static void main(String[] args) {
+        int var01 = 2000;
+        long var02 = 1000;
+        float var03 =605F;
+        double var04 = 10.7;
+        char var05 = '\u0022'; //se asigna a var05 un valor constante unicode
+        char var06 = 'k'; //se asigna a var06 un valor constante caracter
+        boolean var07 = true;
+
+        System.out.println("var01 = "+var01);
+        System.out.println("var02 = "+var02);
+        System.out.println("var03 = "+var03);
+        System.out.println("var04 = "+var04);
+        System.out.println("var05 = "+var05);
+        System.out.println("var06 = "+var06);
+        System.out.println("var07 = "+var07);
+
+
+    }
+}
